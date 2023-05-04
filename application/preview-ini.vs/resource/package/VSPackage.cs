3d9170ea4f9db088cb47cee36fda698224b0f812
Viacheslav Lozinskyi <viacheslav.lozinskyi@gmail.com>
2023-05-04 19:36:49 +0300

    - Minor changes

--- application/preview-ini.vs/resource/package/VSPackage.cs
+++ application/preview-ini.vs/resource/package/VSPackage.cs
@@ -27,7 +27,7 @@ namespace resource.package
             public const string GUID = "0B50E4AD-3B5C-4EAA-96F2-BBEACC945B7C";
             public const string HOST = "MetaOutput";
             public const string NAME = "Preview-INI";
-            public const string VERSION = "1.0.14";
+            public const string VERSION = "1.1.0";
         }
 
         protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
